db344b069fccb21862f85377a4063bd50c22e9f6
Eugene Skepner <git@skepner.eu>
2015-02-03 12:24:47 +0100

    forced en_US.UTF-8 locale for programs executed by emacs

--- eu/eu-env.el
+++ eu/eu-env.el
@@ -2,6 +2,9 @@
     (let ((home (getenv "HOME")))
       (setenv "PATH" (concat home "/bin:" home "/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"))))
 
+(setenv "LANG" "en_US.UTF-8")
+(setenv "LC_ALL" "en_US.UTF-8")
+
 ;----------------------------------------------------------------------
 
 (setenv "TIME" "<Time %E>")
