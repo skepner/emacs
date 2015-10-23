07b9b0060340a0d66d1a5466651a22b44a4c8d7f
Eugene Skepner <git@skepner.eu>
2015-10-23 16:03:36 +0200

    clean up

--- eu/eu-buch.el
+++ eu/eu-buch.el
@@ -2,23 +2,6 @@
 
 (defvar eu-buch-exclude '("." ".." ".git" ".gitignore" ".DS_Store"))
 
-(defun eu-buch-make-menu-old ()
-  (let ((top-dirs
-         (let ((result '()))
-           (dolist (filename (directory-files eu-buch-root t) result)
-             (if (and (not (member (file-name-nondirectory filename) '("." ".." ".git"))) (file-directory-p filename))
-                 (setq result (append result (list filename)))))))
-        (menu)
-        (entries-in (function (lambda (dir)
-                                (let ((r '()))
-                                  (dolist (filename (directory-files dir t) r)
-                                    (if (and (not (file-directory-p filename)) (not (member (file-name-nondirectory filename) '(".DS_Store"))))
-                                        (setq r (append r (list (vector (file-name-nondirectory filename) (list 'find-file filename t) t))))))))))
-        )
-    (dolist (top-dir top-dirs menu)
-      (setq menu (append menu (list (append (list (file-name-nondirectory top-dir)) (funcall entries-in top-dir))))))
-    (prin1 menu)))
-
 ;; ----------------------------------------------------------------------
 
 (defun eu-buch-make-menu ()
