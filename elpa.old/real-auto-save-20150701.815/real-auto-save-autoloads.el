438b6069d781c050d7ed19fafc6692d77b31b30f
Eugene Skepner <github@skepner.eu>
2017-05-02 08:01:23 +0100

    ss

--- elpa.old/real-auto-save-20150701.815/real-auto-save-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; real-auto-save-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("real-auto-save.el") (22578 55090 981145
-;;;;;;  993000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; real-auto-save-autoloads.el ends here
--- elpa.old/real-auto-save-20150701.815/real-auto-save-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "real-auto-save" "20150701.815" "Automatically save your all your buffers/files at regular intervals." 'nil)
